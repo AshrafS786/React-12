5425ec7688d9c93d5cf4b1773088a98cc0876233
Ashraf Ali <ashrafali786522002@gmail.com>
2024-03-20 17:31:08 +0530

    live time

--- React/src/App.jsx
+++ React/src/App.jsx
@@ -1,13 +1,50 @@
-import './App.css'
+// import { useState } from "react";
 
-function App() {
+// const App = () => {
+// let [age, setAge] = useState(12)
+
+// const changeAge = () => {
+//   const newAge = 15;
+//   setAge(newAge);
+// }
+
+//   return (
+//     <>
+//       <h1>{age}</h1>
+//       <button onClick={changeAge}>Change Age</button>
+//     </>
+//   )
+// }
+
+// export default App
+
+
+import { useState } from 'react';
+
+function Clock() {
+  const [time, setTime] = useState(new Date());
+
+  setInterval(() =>{
+    setTime(new Date());
+  }, 1000);
+
+  // useEffect(() => {
+  //   const timerID = setInterval(() => tick(), 1000);
+  //   return () => {
+  //     clearInterval(timerID);
+  //   };
+  // }, []);
+
+  // function tick() {
+  //   setTime(new Date());
+  // }
 
   return (
-    <>
-      
-      <div>hello</div>
-    </>
-  )
+    <div>
+      <h1>Live Clock</h1>
+      <h2>It is {time.toLocaleTimeString()}.</h2>
+    </div>
+  );
 }
 
-export default App
+export default Clock;
