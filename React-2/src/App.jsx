132b7696f0968ca3e4d27f86e561e7ec551c7dea
Ashraf Ali <ashrafali786522002@gmail.com>
2024-04-02 17:25:49 +0530

    updated form two way

--- React-2/src/App.jsx
+++ React-2/src/App.jsx
@@ -1,35 +1,73 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState } from "react";
 
+// const App = () => {
+//   const [username, setUsername] = useState("John");
+
+// // const wrapper === () => changeUsername("Jane");
+
+//   const changeUsername = (x) => {
+//     setUsername(x);
+//   }
+// console.log(username);
+
+//   return (
+//     <>
+//       <nav>
+//         <h1>{username}</h1>
+//       </nav>
+//       <section>
+//         <button onClick={() => changeUsername("Jane")}>Change User</button>
+//         <p>This is section</p>
+//       </section>
+//       <footer>
+//         <p>This is footer</p>
+//       </footer>
+//     </>
+//   )
+// }
+
+// export default App
+
+const App = () => {
+const [username, setUsername] = useState("");
+const [email, setEmail] = useState("");
+const [password, setPassword] = useState();
+
+  const submitButton = (e) => {
+    e.preventDefault();
+    console.log({username, email, password});
+    // console.log(e.target[0].value);
+    // console.log(e.target[1].value);
+    // console.log(e.target[2].value);
+  };
+
+//   const ChangeHandler = (e) => setUsername(e.target.value)
+  
+//   const ChangeHandler = (e) => setEmail(e.target.value)
+  
+//   const ChangeHandler = (e) => setPassword(e.target.value)
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={submitButton}>
+        <label htmlFor="text">Username: </label>
+        <input onChange={(e) => setUsername(e.target.value)} type="text" placeholder="username" value={username} />
+        <br />
+        <br />
+        <label htmlFor="email">Email: </label>
+        <input onChange={(e) => setEmail(e.target.value)} type="email" placeholder="email" value={email} />
+        <br />
+        <br />
+        <label htmlFor="password">Password: </label>
+        <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="*******" value={password} />
+        <br />
+        <br />
+        <button>Sumit</button>
+      </form>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
